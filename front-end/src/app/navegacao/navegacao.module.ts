6f1bbc29267df95c72689940912b42bb46d9a316
Igor Daflon do Couto <igordafloncouto@gmail.com>
2024-02-24 21:02:39 -0300

    Adiciona bootstrap

--- front-end/src/app/navegacao/navegacao.module.ts
+++ front-end/src/app/navegacao/navegacao.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./home/home.component";
 import { MenuComponent } from "./menu/menu.component";
@@ -15,7 +17,8 @@ import { NotFoundComponent } from "./not-found/not-found.component";
         NotFoundComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        NgbModule
     ],
     exports: [
         MenuComponent,
