64bf281ce22c1e286fc1505f78d9c4d92e4fb153
Igor Daflon do Couto <igordafloncouto@gmail.com>
2024-02-25 15:17:45 -0300

    Adiciona Environments

--- front-end/src/app/services/base.service.ts
+++ front-end/src/app/services/base.service.ts
@@ -1,11 +1,12 @@
 import { HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import { throwError } from "rxjs";
+import { environment } from "src/environments/environment.development";
 import { LocalStorageUtils } from "../utils/localstorage";
 
 export abstract class BaseService {
 
     public LocalStorage = new LocalStorageUtils();
-    protected UrlServiceV1: string = "https://localhost:44321/api/v1/";
+    protected UrlServiceV1: string = environment.apiUrl;
 
     protected ObterHeaderJson() {
         return {
